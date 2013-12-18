c283a695beb94bd2bb2987e342d1e1ed07fee6a1
Niklas Hökenström <niklashokenstrom@gmail.com>
2013-12-18 12:23:38 +0100

    la till exempel

---
+++ exempel.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int x = 7;
+  int i = 2;
+
+  if (i = 1) {
+    printf("1");
+  } else {
+    printf("2");
+  }
+
+  for(i = 1; i >= i; i) {
+    printf("New Line \n");
+  }
+
+}
